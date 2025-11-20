30a5eb9d2124c467500b72a035f1d4db712cc701
Admin <tejashwinibawane@gmail.com>
2025-11-21 00:27:47 +0530

    success

--- pipeline.groovy
+++ pipeline.groovy
@@ -1,32 +1,3 @@
-// pipeline {
-//     agent {label 'slave'}
-//     stages {
-//         stage('pull-stage') {
-//             steps {
-//                 git branch: 'main', url: 'https://github.com/Anilbamnote/student-ui-app.git'
-//             }
-//         }
-//         stage('Build') {
-//             steps {
-//                 sh '/opt/maven/bin/mvn clean package'
-//             }
-//         }
-//         stage('test') {
-//             steps {
-//                 echo "test suecss"
-//             }
-//         }
-//         stage('deploy') {
-//             steps {
-//                 echo "deploy sucess"
-//             }
-//         }
-//     }
-// }
-
-
-
-
 pipeline {
     agent any
     stages {
